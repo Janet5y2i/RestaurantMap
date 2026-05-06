5dd7ea91c7f5efe6030b54e73586797e8c424ea0
Janet5y2i <janet.chang.0110@gmail.com>
2026-05-06 19:36:41 +1000

    update version

--- FoodMapServer/obj/Debug/net10.0/FoodMapServer.AssemblyInfo.cs
+++ FoodMapServer/obj/Debug/net10.0/FoodMapServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodMapServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e62aef7973104815277f46ed3eb2da2ac3f3491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6778191a72a090b83bbee37f0ae94c68851da89b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodMapServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodMapServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
